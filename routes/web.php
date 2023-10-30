65196c963a680cc3914f7614507c43ce18f45880
deaabeliya <dblyaaaa@gmail.com>
2023-10-30 19:25:34 +0700

    video 4 Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Dea Abeliya",
         "email" => "deaabeliya@unpas.ac.id",
         "image" => "1.jpg"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Black Hole",
+            "slug" => "black-hole",
+            "author" => "Dea Abeliya",
+            "body" => "A black hole is a region in space where gravity is so strong that nothing, including light or other electromagnetic waves, can escape it. They form from the remnants of a large star that dies in a supernova explosion, and the resulting gravitational field is incredibly powerful. Key features of a black hole include the event horizon, which is the boundary in spacetime through which matter and light can only pass inward towards the mass of the black hole, and the singularity, which is the point at the center of a black hole where spacetime curvature becomes infinite."
+        ],
+        [
+            "title" => "Aurora",
+            "slug" => "aurora",
+            "author" => "Gilang Syaputra",
+            "body" => "Aurora is a natural light display in Earth's sky, predominantly seen in high-latitude regions. It is also known as the northern lights (aurora borealis) or southern lights (aurora australis). Aurora is caused by the ionization and excitation of atmospheric constituents leading to auroral emissions."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Black Hole",
+            "slug" => "black-hole",
+            "author" => "Dea Abeliya",
+            "body" => "A black hole is a region in space where gravity is so strong that nothing, including light or other electromagnetic waves, can escape it. They form from the remnants of a large star that dies in a supernova explosion, and the resulting gravitational field is incredibly powerful. Key features of a black hole include the event horizon, which is the boundary in spacetime through which matter and light can only pass inward towards the mass of the black hole, and the singularity, which is the point at the center of a black hole where spacetime curvature becomes infinite."
+        ],
+        [
+            "title" => "Aurora",
+            "slug" => "aurora",
+            "author" => "Gilang Syaputra",
+            "body" => "Aurora is a natural light display in Earth's sky, predominantly seen in high-latitude regions. It is also known as the northern lights (aurora borealis) or southern lights (aurora australis). Aurora is caused by the ionization and excitation of atmospheric constituents leading to auroral emissions."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
