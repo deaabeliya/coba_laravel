86cd4a4ac14a0eca2ff1d0878c9673a53fb9125f
deaabeliya <dblyaaaa@gmail.com>
2023-11-29 17:02:37 +0700

    Video 18 Create Post Form

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <div class="row justify-content-center mb-3">
   <div class="col-md-6">
-    <form action="/posts">
+    <form action="/blog">
       @if (request('category'))
         <input type="hidden" name="category" value="{{ request('category') }}">
       @endif
@@ -26,26 +26,19 @@
 
 @if ($posts->count())
 <div class="card mb-3">
-    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" 
-    class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
     <div class="card-body text-center">
-      <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
-      class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+      <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
       <p>
         <small class="text-muted">
-            By. <a href="/posts?author={{ $posts[0]->author->username }}" 
-            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]
-            ->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+            By. <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
         </small>
       </p>
 
       <p class="card-text">{{ $posts[0]->excerpt }}</p>
 
-      <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read 
-      more</a>
+      <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
     
-        
-     
     </div>
   </div>
 
@@ -55,18 +48,13 @@
         @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
             <div class="card">
-                <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)
-                "><a href="/posts?category={{ $post->category->slug}}" class="text-white 
-                text-decoration-none">{{ $post->category->name }}</a></div>
-                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" 
-                class="card-img-top" alt="{{ $post->category->name }}">
+                <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/blog?category={{ $post->category->slug}}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                   <p>
                     <small class="text-muted">
-                        By. <a href="/posts?author={{ $post->author->username }}" 
-                        class="text-decoration-none">{{ $post->author->name }}</a> {{ 
-                        $post->created_at->diffForHumans() }}
+                        By. <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                     </small>
                   </p>
                   <p class="card-text">{{ $post->excerpt }}</p>
@@ -86,7 +74,7 @@
   {{ $posts->links() }}
 </div>
 
-
-
 @endsection
+
+ 
     
